81ed548f145589b0e3d1fe09b425b5df063c73fa
CaptainCake <53238017+FedotovN@users.noreply.github.com>
2022-11-27 21:40:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const doc = document,
+      inputBar = doc.getElementById('input'),
+      container = doc.getElementById('pic_container')
+
+inputBar.addEventListener(('keydown'), e=>{
+    if(e.key == "Enter"){
+        fillContainer(inputBar.value)
+        console.log("yapi")
+    }
+})
+
+function fillContainer(req){
+    deletePictures()
+    const url = 'https://api.unsplash.com/search/photos?query='+req+'&per_page=12&client_id=qmpjS21eSX5aNbUiKxmhc_LEe0nMhdlhL-32uqeuS7g'
+    fetch(url)
+  .then(response => {
+    if (!response.ok) throw Error(response.statusText);
+      return response.json();
+   })
+
+   .then(data => {
+      //loadImages(data);
+   })
+
+   .catch(error => console.log(error));   
+}
+
+
+function deletePictures(){
+    container.innerHTML = ""
+}
+
+      
